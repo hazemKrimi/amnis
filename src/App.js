69f1d8d30c648f28f6515a51716385811df3d109
Hazem Krimi <krimihazem1@gmail.com>
2020-05-28 03:26:20 +0100

    Added routing and loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { lazy, Suspense } from 'react';
+import MainContextProvider from './contexts/MainContext';
+import { Switch, Route } from 'react-router-dom';
+import GlobalStyles from './components/GlobalStyles';
+import Loader from './components/Loader';
+
+const Landing = lazy(() => import('./pages/Landing'));
 
 const App = () => {
   return (
-    <div></div>
+    <MainContextProvider>
+      <GlobalStyles />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route>
+            <Landing />
+          </Route>
+        </Switch>
+      </Suspense>
+    </MainContextProvider>
   );
 }
 
