00dfc11cca968bcbee777feda5e6b568783311b6
Hazem Krimi <krimihazem1@gmail.com>
2020-05-29 05:23:57 +0100

    Changed fonts import location

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -2,25 +2,27 @@ import React, { useContext } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { MainContext } from '../contexts/MainContext';
 
+const Styles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        user-select: none;
+        outline: none;
+        white-space: nowrap;
+        font-family: 'Open Sans';
+        color: ${({ darkMode }) => darkMode ? '#FFFFFF' : '#07070A'};
+    }
+
+    body {
+        background-color: ${({ darkMode }) => darkMode ? '#24272B' : '#F3F4F9'};
+    }
+`;
+
 const GlobalStyles = () => {
     const { darkMode } = useContext(MainContext);
 
-    const Styles = createGlobalStyle`
-        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Poppins:wght@700&display=swap');
-
-        * {
-            margin: 0;
-            padding: 0;
-        }
-
-        body {
-            background-color: ${darkMode ? '#24272B' : '#F3F4F9'};
-            color: ${darkMode ? '#FFFFFF' : '#07070A'};
-            font-family: 'Open Sans';
-        }
-    `;
-
-    return <Styles />
+    return <Styles darkMode={darkMode}/>
 };
 
 export default GlobalStyles;
