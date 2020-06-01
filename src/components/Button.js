9d4af95f0348692452cd9a1f745e7baab9411f4b
Hazem Krimi <krimihazem1@gmail.com>
2020-06-01 16:47:02 +0100

    Changed button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,16 +5,17 @@ const Btn = styled.button`
     cursor: pointer;
     border: none;
     box-sizing: border-box;
-    color: ${({ mode }) => mode === 'action' ? '#FFFFFF' : 'inherit'};
-    background: ${({ mode }) => mode === 'action' ? '#FF4A4A' : 'none'};
-    font-weight: ${({ mode }) => mode === 'action' ? '400' : '600'};
+    color: ${({ mode }) => mode === 'action' || mode === 'form' ? '#FFFFFF' : 'inherit'};
+    background: ${({ mode }) => mode === 'action' || mode === 'form' ? '#FF4A4A' : 'none'};
+    font-weight: ${({ mode }) => mode === 'action' || mode === 'form' ? '400' : '600'};
+    width: ${({ mode }) => mode === 'form' ? '100%' : 'initial'};
     font-size: 1rem;
-    padding: 0.5rem 2rem;
+    padding: ${({ mode }) => mode === 'form' ? '1rem 2rem' : '0.5rem 2rem'};
 `;
 
 const Button = ({ text, mode, onClick }) => {
     return ( 
-        <Btn mode={mode} onClick={onClick}>{text}</Btn>
+        <Btn mode={mode} onClick={() => onClick()}>{text}</Btn>
     );
 }
  
