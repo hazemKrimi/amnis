6e8191de787c6015e9f0119b57a93c5adc562992
Hazem Krimi <krimihazem1@gmail.com>
2020-07-17 03:05:16 +0100

    Added danger button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,12 +5,13 @@ const Btn = styled.button`
     cursor: pointer;
     border: none;
     box-sizing: border-box;
-    color: ${({ mode }) => mode === 'action' || mode === 'form' ? '#FFFFFF' : 'inherit'};
+    color: ${({ mode }) => mode === 'action' || mode === 'form' ? '#FFFFFF' : mode === 'danger' ? '#FF4A4A' : 'inherit'};
     background: ${({ mode }) => mode === 'action' || mode === 'form' ? '#FF4A4A' : 'none'};
     font-weight: ${({ mode }) => mode === 'action' || mode === 'form' ? '400' : '600'};
+    border: ${({ mode }) => mode === 'danger' ? '1px solid #FF4A4A' : 'none'};;
     width: ${({ mode }) => mode === 'form' ? '100%' : 'initial'};
     font-size: 1rem;
-    padding: ${({ mode }) => mode === 'form' ? '1rem 2rem' : '0.5rem 2rem'};
+    padding: ${({ mode }) => mode === 'form' || mode === 'danger' ? '1rem 2rem' : '0.5rem 2rem'};
     transition: none;
 `;
 
