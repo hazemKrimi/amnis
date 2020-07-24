78b9a7beab023e144a94fbfb9161c80187a8c55d
Hazem Krimi <krimihazem1@gmail.com>
2020-07-24 02:22:16 +0100

    Added update user action

--- src/contexts/reducers/userReducer.js
+++ src/contexts/reducers/userReducer.js
@@ -1,12 +1,14 @@
 export const SIGN_UP = 'LOG_IN';
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
+export const UPDATE_USER = 'UPDATE_USER';
 
 export const reducer = (state, { type, payload }) => {
     switch (type) {
         case SIGN_UP: return { ...state, user: payload };
         case LOG_IN: return { ...state, user: payload };
         case LOG_OUT: return { ...state, user: null };
+        case UPDATE_USER: return { ...state, user: payload };
         default: return state;
     }
 }
