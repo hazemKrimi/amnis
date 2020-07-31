52287974555538a4f8bc8556ebcbfd55c36a5016
Hazem Krimi <krimihazem1@gmail.com>
2020-07-31 17:07:24 +0100

    Added delete user action

--- src/contexts/reducers/userReducer.js
+++ src/contexts/reducers/userReducer.js
@@ -2,6 +2,7 @@ export const SIGN_UP = 'LOG_IN';
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
 export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'UPDATE_USER';
 
 export const reducer = (state, { type, payload }) => {
     switch (type) {
@@ -9,6 +10,7 @@ export const reducer = (state, { type, payload }) => {
         case LOG_IN: return { ...state, user: payload };
         case LOG_OUT: return { ...state, user: null };
         case UPDATE_USER: return { ...state, user: payload };
+        case DELETE_USER: return { ...state, user: null };
         default: return state;
     }
 }
