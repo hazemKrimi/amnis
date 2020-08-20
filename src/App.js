d9df8f6493effee1774fbb85127033c9ef97cbd3
Hazem Krimi <krimihazem1@gmail.com>
2020-08-21 00:27:16 +0100

    Moved global state to index.js and added initial loading to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { lazy, Suspense } from 'react';
-import MainContextProvider from './contexts/MainContext';
-import UserContextProvider from './contexts/UserContext';
+import React, { lazy, Suspense, useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { UserContext } from './contexts/UserContext';
 import GlobalStyles from './components/GlobalStyles';
 import Loader from './components/Loader';
+import InitialLoader from './components/InitialLoader';
 
 const Nav = lazy(() => import('./components/Nav'));
 const Landing = lazy(() => import('./pages/Landing'));
@@ -11,26 +11,34 @@ const Settings = lazy(() => import('./pages/Settings'));
 const Upload = lazy(() => import('./pages/Upload'));
 
 const App = () => {
+  const { initialLoading } = useContext(UserContext);
+
   return (
-    <MainContextProvider>
-      <UserContextProvider>
-        <GlobalStyles />
-        <Suspense fallback={<Loader />}>
-          <Nav />
-          <Switch>
-            <Route path='/' exact>
-              <Landing />
-            </Route>
-            <Route path='/settings' exact>
-              <Settings />
-            </Route>
-            <Route path='/upload' exact>
-              <Upload />
-            </Route>
-          </Switch>
-        </Suspense>
-      </UserContextProvider>
-    </MainContextProvider>
+    <>
+      {
+        initialLoading ? (
+          <InitialLoader />
+        ) : (
+          <>
+            <GlobalStyles />
+            <Suspense fallback={<Loader />}>
+              <Nav />
+              <Switch>
+                <Route path='/' exact>
+                  <Landing />
+                </Route>
+                <Route path='/settings' exact>
+                  <Settings />
+                </Route>
+                <Route path='/upload' exact>
+                  <Upload />
+                </Route>
+              </Switch>
+            </Suspense>
+          </>
+        )
+      }
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MainContextProvider from './contexts/MainContext';
+import UserContextProvider from './contexts/UserContext';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MainContextProvider>
+      <UserContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserContextProvider>
+    </MainContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
