a3ca9773eb770f1a66f6d880ca36e5b41286ad06
Hazem Krimi <krimihazem1@gmail.com>
2020-08-27 17:17:51 +0100

    Added videos to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { MainContext } from '../contexts/MainContext';
 import styled from 'styled-components';
+import Alert from '../components/Alert';
 
 const Container = styled.div`
     width: 90%;
@@ -12,9 +14,24 @@ const Container = styled.div`
 `;
 
 const Landing = () => {
+    const { getVideos } = useContext(MainContext);
+    const [ alert, setAlert ] = useState(null);
+
+    useEffect(() => {
+        (async() => {
+            try {
+                await getVideos();
+            } catch(err) {
+                setAlert({ type: 'failure', text: 'Error occured! Try again later' });
+                setTimeout(() => setAlert(null), 5000);
+            }
+        })();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <Container>
-            <h2>Live</h2>
+            { alert && <Alert type={alert.type} text={alert.text} /> }
             <h2>Recent Videos</h2>
         </Container>
     )
