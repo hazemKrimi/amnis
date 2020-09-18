b72fc10d6aae5034721bc66d88004b5a543a92c3
Hazem Krimi <krimihazem1@gmail.com>
2020-09-18 02:04:50 +0100

    Added player and loading to video page

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { MainContext } from '../contexts/MainContext';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import Player from '../components/Player';
+import Loader from '../components/Loader';
+
+const Container = styled.div`
+    width: 90%;
+    margin: 3rem auto;
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 1.5rem;
+`;
 
 const Video = () => {
-    return (
-        <>
-        </>
-    );
+    const { getVideo } = useContext(MainContext);
+    const { id } = useParams();
+    const [ loading, setLoading ] = useState(false);
+    const [ video, setVideo ] = useState(null);
+
+    useEffect(() => {
+        (async() => {
+            try {
+                setLoading(true);
+                setVideo(await getVideo(id));
+                setLoading(false);
+            } catch(err) {
+                setLoading(false);
+                alert(err);
+            }
+        })();
+        // eslint-disable-next-line
+    }, []);
+
+    return !loading ? (
+        <Container>
+            <Player
+                autoplay={false}
+                controls={true}
+                sources={[
+                    { 
+                        src: video && video.source,
+                        type: 'video/mp4'
+                    }
+                ]}
+            />
+        </Container>
+    ) : <Loader />;
 }
  
 export default Video;
