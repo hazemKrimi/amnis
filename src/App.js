0a2488b2e8e40f8b1d6b911ee696b1ad201e0afa
Hazem Krimi <krimihazem1@gmail.com>
2020-09-21 19:26:28 +0100

    Added offline functionality to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { lazy, Suspense, useContext } from 'react';
+import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 import { UserContext } from './contexts/UserContext';
+import { MainContext } from './contexts/MainContext';
 import GlobalStyles from './components/GlobalStyles';
 import Loader from './components/Loader';
 import InitialLoader from './components/InitialLoader';
@@ -12,8 +14,22 @@ const Upload = lazy(() => import('./pages/Upload'));
 const Search = lazy(() => import('./pages/Search'));
 const Video = lazy(() => import('./pages/Video'));
 
+const Offline = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: auto;
+  justify-content: center;
+  align-items: center;
+
+  h1, h3 {
+    font-family: 'Poppins';
+    text-align: center;
+  }
+`;
+
 const App = () => {
   const { initialLoading } = useContext(UserContext);
+  const { offline } = useContext(MainContext);
 
   return (
     <>
@@ -24,24 +40,37 @@ const App = () => {
           <>
             <GlobalStyles />
             <Suspense fallback={<Loader />}>
-              <Nav />
-              <Switch>
-                <Route path='/' exact>
-                  <Landing />
-                </Route>
-                <Route path='/settings' exact>
-                  <Settings />
-                </Route>
-                <Route path='/upload' exact>
-                  <Upload />
-                </Route>
-                <Route path='/search/:query' exact>
-                  <Search />
-                </Route>
-                <Route path='/video/:id' exact>
-                  <Video />
-                </Route>
-              </Switch>
+              {
+                !offline ? (
+                  <>
+                    <Nav />
+                    <Switch>
+                      <Route path='/' exact>
+                        <Landing />
+                      </Route>
+                      <Route path='/settings' exact>
+                        <Settings />
+                      </Route>
+                      <Route path='/upload' exact>
+                        <Upload />
+                      </Route>
+                      <Route path='/search/:query' exact>
+                        <Search />
+                      </Route>
+                      <Route path='/video/:id' exact>
+                        <Video />
+                      </Route>
+                    </Switch>
+                  </>
+                ) : (
+                  <Offline>
+                    <div>
+                      <h1>You are currently offline</h1>
+                      <h3>Check your internet connection and try again</h3>
+                    </div>
+                  </Offline>
+                )
+              }
             </Suspense>
           </>
         )
