97bc16e794e1c66274ae11c78472fc3ff6f41f22
Hazem Krimi <krimihazem1@gmail.com>
2020-10-01 21:19:05 +0100

    Added env variables to firebase config properly

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/storage';
 import 'firebase/auth';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyBfG3yrOOKbBPdCjvSs6zrp_PMw1Fh07ik",
-    authDomain: "amnis-bc042.firebaseapp.com",
-    databaseURL: "https://amnis-bc042.firebaseio.com",
-    projectId: "amnis-bc042",
-    storageBucket: "amnis-bc042.appspot.com",
-    messagingSenderId: "643844816586",
-    appId: "1:643844816586:web:47e10121fe3bd40503be33"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
